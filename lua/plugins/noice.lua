5c1bde80bb462cfb945f768d03e7ab723a0c5595
Ahmed Hegazi <73020478+A-Hegazi@users.noreply.github.com>
2026-08-16 12:47:33 +0300

    Disable Noice signature help handling
    Disable signature help handling by Noice to allow blink.cmp to manage it.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -8,11 +8,17 @@ return {
     },
     opts = {
       lsp = {
+        -- Let blink.cmp handle signature help instead of Noice.
+        signature = {
+          enabled = false,
+        },
+
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
         },
       },
+
       presets = {
         bottom_search = true,
         command_palette = true,
